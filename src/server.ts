520c62867e1537559c53d8ef18882b0198637e6e
borhansahed <mdsahed1099@gmail.com>
2023-06-27 11:13:12 +0600

    test husky

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,17 @@
 /* eslint-disable no-console */
 
 import express, { Application, Response, Request } from 'express'
+import cors from 'cors'
 
 const port = process.env.PORT || 5000
 const app: Application = express()
 
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.get('/', (req: Request, res: Response) => {
   res.send('Welcome to my Digital Cow Server')
 })
-
-app.use(express.json())
 
 app.listen(5000, () => {
   console.log(`app running on ${port}`)
